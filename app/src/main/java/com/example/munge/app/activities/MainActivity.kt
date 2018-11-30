6b2eb701baace1ab8f0542a0bb5ffd176f706649
MagnusGreiff <mangegreiff@gmail.com>
2018-11-30 11:42:46 +0100

    Added toolbar.

--- app/src/main/java/com/example/munge/app/activities/MainActivity.kt
+++ app/src/main/java/com/example/munge/app/activities/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.munge.app.activities
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
 import com.example.munge.app.R
@@ -14,19 +16,36 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //setting toolbar
+        setSupportActionBar(findViewById(R.id.toolbar))
+        //home navigation
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
-//
-    fun clickButton(view: View) {
-        val meh = arrayOf(1,2, "hej")
-        val text = Toast.makeText(this, Arrays.toString(meh), Toast.LENGTH_SHORT)
-        text.show()
+    //setting menu in action bar
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.my_menu,menu)
+        return super.onCreateOptionsMenu(menu)
     }
 
+    // actions on click menu items
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        R.id.action_settings -> {
+            // User chose the "Print" item
+            val intent = Intent(this, SettingsActivity::class.java)
+            startActivity(intent)
+//            Toast.makeText(this,"Print action",Toast.LENGTH_LONG).show()
+            true
+        }
+        android.R.id.home ->{
+            Toast.makeText(this,"Home action",Toast.LENGTH_LONG).show()
+            true
+        }
 
-//    fun changeActivity() {
-//        val intent = Intent(this, SecondActivity::class.java)
-//        intent.putExtra("keyIdentifier", "test")
-//        startActivity(intent)
-//    }
+        else -> {
+            // If we got here, the user's action was not recognized.
+            // Invoke the superclass to handle it.
+            super.onOptionsItemSelected(item)
+        }
+    }
 }
