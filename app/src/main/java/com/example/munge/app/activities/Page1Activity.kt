7701b4418aa7a4026707211eb1d1136a8c84719d
André Johansson <anjd16@student.bth.se>
2018-12-04 12:45:14 +0100

    Added title, input and button

---
+++ app/src/main/java/com/example/munge/app/activities/Page1Activity.kt
@@ -0,0 +1,13 @@
+package com.example.munge.app.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.munge.app.R
+
+class Page1Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_page1)
+    }
+}
