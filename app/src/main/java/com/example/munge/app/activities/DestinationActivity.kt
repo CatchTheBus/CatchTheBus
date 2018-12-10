cc4554f9463b647f45e11989f55cbe1967e2b882
André Johansson <anjd16@student.bth.se>
2018-12-10 13:40:16 +0100

    Added dynamic list for destinations

--- app/src/main/java/com/example/munge/app/activities/DestinationActivity.kt
+++ app/src/main/java/com/example/munge/app/activities/DestinationActivity.kt
@@ -2,12 +2,16 @@ package com.example.munge.app.activities
 
 import android.content.Intent
 import android.os.Bundle
+import android.support.design.widget.TextInputEditText
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.TextView
 import android.widget.Toast
 import com.example.munge.app.R
+import kotlinx.android.synthetic.main.activity_destination.*
 
 class DestinationActivity : AppCompatActivity() {
 
@@ -18,6 +22,22 @@ class DestinationActivity : AppCompatActivity() {
         setSupportActionBar(findViewById(R.id.toolbar))
         //home navigation
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        // Set header to location name
+        findViewById<TextView>(R.id.location).text = intent.extras["search"].toString()
+
+        addBuses()
+        addDestinations()
+        addTimes()
+
+        // Creates a vertical Layout Manager
+        bus_list.layoutManager = LinearLayoutManager(this)
+
+         // You can use GridLayoutManager if you want multiple columns. Enter the number of columns as a parameter.
+//        rv_animal_list.layoutManager = GridLayoutManager(this, 3)
+
+        // Access the RecyclerView Adapter and load the data into it
+        bus_list.adapter = BusAdapter(buses, bus_destinations, bus_times, this)
     }
 
     //setting menu in action bar
@@ -45,5 +65,32 @@ class DestinationActivity : AppCompatActivity() {
             // Invoke the superclass to handle it.
             super.onOptionsItemSelected(item)
         }
+    }
+
+    val buses: ArrayList<String> = ArrayList()
+
+    fun addBuses() {
+        buses.add("441")
+        buses.add("440")
+        buses.add("444")
+        buses.add("470")
+    }
+
+    val bus_destinations: ArrayList<String> = ArrayList()
+
+    fun addDestinations() {
+        bus_destinations.add("To: Stockholm")
+        bus_destinations.add("To: Stockholm")
+        bus_destinations.add("To: Stockholm")
+        bus_destinations.add("To: Stockholm")
+    }
+
+    val bus_times: ArrayList<String> = ArrayList()
+
+    fun addTimes() {
+        bus_times.add("TTD: 5 min")
+        bus_times.add("TTD: 5 min")
+        bus_times.add("TTD: 5 min")
+        bus_times.add("TTD: 5 min")
     }
 }
