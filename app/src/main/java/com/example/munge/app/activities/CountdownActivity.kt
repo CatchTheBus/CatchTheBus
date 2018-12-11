74bb75232d2d398f7232c373d4c2bbcd3f5c0d08
André Johansson <anjd16@student.bth.se>
2018-12-11 18:18:47 +0100

    Created countdown timer
    Linked countdown activity to destinations (when clicking on bus number)
    Added ability to send data from destinations to countdown
    Created countdown timer with hardcoded departure time

--- app/src/main/java/com/example/munge/app/activities/CountdownActivity.kt
+++ app/src/main/java/com/example/munge/app/activities/CountdownActivity.kt
@@ -2,14 +2,24 @@ package com.example.munge.app.activities
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.TextView
 import android.widget.Toast
 import com.example.munge.app.R
+import kotlinx.android.synthetic.main.activity_countdown.*
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.concurrent.TimeUnit
 
 
 class CountdownActivity : AppCompatActivity() {
+
+    private var isCancelled = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +31,62 @@ class CountdownActivity : AppCompatActivity() {
         //home navigation
         setSupportActionBar(findViewById(R.id.toolbar))
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        findViewById<TextView>(R.id.countdown_header).text = intent.extras["bus"].toString()
+
+        val currentTime = Calendar.getInstance().getTime()
+
+        val dtStart = "2018-12-11T18:40:00"
+        val format = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss")
+        val date = format.parse(dtStart)
+        val millisInFuture = date.time - currentTime.time
+
+        val countDownInterval:Long = 1000
+
+        timer(millisInFuture, countDownInterval).start()
+
+        isCancelled = false
+    }
+
+    // Method to configure and return an instance of CountDownTimer object
+    private fun timer(millisInFuture:Long,countDownInterval:Long):CountDownTimer{
+        val text_view = findViewById<TextView>(R.id.countdown_view)
+        return object: CountDownTimer(millisInFuture,countDownInterval){
+            override fun onTick(millisUntilFinished: Long){
+                val timeRemaining = timeString(millisUntilFinished)
+                if (isCancelled){
+                    text_view.text = "${text_view.text}\nStopped.(Cancelled)"
+                    cancel()
+                } else {
+                    text_view.text = timeRemaining
+                }
+            }
+
+            override fun onFinish() {
+                text_view.text = "The bus left"
+            }
+        }
+    }
+
+    private fun timeString(millisUntilFinished:Long):String{
+        var millisUntilFinished:Long = millisUntilFinished
+        val days = TimeUnit.MILLISECONDS.toDays(millisUntilFinished)
+        millisUntilFinished -= TimeUnit.DAYS.toMillis(days)
+
+        val hours = TimeUnit.MILLISECONDS.toHours(millisUntilFinished)
+        millisUntilFinished -= TimeUnit.HOURS.toMillis(hours)
+
+        val minutes = TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished)
+        millisUntilFinished -= TimeUnit.MINUTES.toMillis(minutes)
+
+        val seconds = TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished)
+
+        // Format the string
+        return String.format(
+                Locale.getDefault(),
+                "%02d:%02d:%02d:%02d",
+                days, hours, minutes, seconds
+        )
     }
 
     //setting menu in action bar
