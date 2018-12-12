c3f9d9b98f90614c85c6db86b520bcf50b2702f2
Ylva Wright <Ylva.Wright@cybercom.com>
2018-12-12 11:52:21 +0000

    positioned settingsbuttons

--- app/src/main/java/com/example/munge/app/activities/AppSettingsActivity.kt
+++ app/src/main/java/com/example/munge/app/activities/AppSettingsActivity.kt
@@ -1,65 +1,72 @@
 package com.example.munge.app.activities
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.Menu
 import com.example.munge.app.R
 import android.graphics.Color
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.TextView
+import com.example.munge.app.R.id.*
 import kotlinx.android.synthetic.main.activity_settings.*
 
 
 
-// https://android--code.blogspot.com/2018/04/android-kotlin-countdowntimer-days.html
-class AppSettingsActivity : AppCompatPreferenceActivity() {
+class AppSettingsActivity : AppCompatPreferenceActivity(), AdapterView.OnItemSelectedListener {
+
+    // Drop down notifications vars
+    var notification_alert_choice = arrayOf("notifications off", "5 min", "10 min", "20 min", "30 min")
+    var spinner:Spinner? = null
+    var textView_msg: TextView? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
-        /*
-        // Set an checked change listener for switch button
-        switch_button.setOnCheckedChangeListener { buttonView, isChecked ->
+        // Set an checked change listener for switch buttons
+        switch_button_gps.setOnCheckedChangeListener { buttonView, isChecked ->
             if (isChecked) {
-                // The switch is enabled/checked
-                val text_view.text = "Switch on"
-
                 // Change the app background color
-                root_layout.setBackgroundColor(Color.LTGRAY)
+                root_layout.setBackgroundColor(Color.DKGRAY)
             } else {
-                // The switch is disabled
-                val text_view.text = "Switch off"
-
                 // Set the app background color to light gray
                 root_layout.setBackgroundColor(Color.LTGRAY)
             }
         }
-        */
-
-        /*
-        // Set a click listener for root layout object switch-button
-        root_layout.setOnClickListener{
-            // Get the switch button state programmatically
-            if(switch_button.isChecked){
-                // If switch button is checked/on then
-                // The switch is enabled/checked
-                val text_view.text = "Switch is on"
-
-                // Change the app background color
-                root_layout.setBackgroundColor(Color.GREEN)
-            }else{
-                // The switch is unchecked
-                val text_view.text = "Switch is off"
-
-                // Set the app background color to light gray
-                root_layout.setBackgroundColor(Color.LTGRAY)
-            }
-        }
-        */
-
-
         // Set a click listener for the button widget
-        button.setOnClickListener{
+        switch_button_gps.setOnClickListener{
             // Change the switch button checked state on button click
-            switch_button.isChecked = if(switch_button.isChecked)false else true
+            switch_button_gps.isChecked = if(switch_button_gps.isChecked)false else true
         }
+
+
+
+        // Drop down notifications
+        textView_msg = this.spinner_msg
+
+        spinner = this.notifications_spinner
+        spinner!!.setOnItemSelectedListener(this)
+
+        // Create an ArrayAdapter using a simple spinner layout and languages array
+        val aa = ArrayAdapter(this, android.R.layout.simple_spinner_item, notification_alert_choice)
+        // Set layout to use when the list of choices appear
+        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        // Set Adapter to Spinner
+            spinner!!.setAdapter(aa)
+
+
+
+    }
+
+    override fun onItemSelected(arg0: AdapterView<*>, arg1: View, position: Int, id: Long) {
+        textView_msg!!.text = "notifications interval: "+notification_alert_choice[position]
+    }
+
+    override fun onNothingSelected(arg0: AdapterView<*>) {
+
     }
 }
